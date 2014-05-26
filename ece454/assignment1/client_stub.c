1cca1e752e87c205d71d39b6eb7cab203481ba0c
vcoste <vincentcoste_4@hotmail.com>
2014-05-25 23:01:25 -0400

    client_stub compiles, it has errors but main functionality is there. Missing argument list parsing

--- ece454/assignment1/client_stub.c
+++ ece454/assignment1/client_stub.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <stdarg.h>
+
 #include "ece454rpc_types.h"
 
 #define BUF_SIZE 1024
@@ -26,13 +35,15 @@ return_type make_remote_call(	const char *servernameorip,
     host = gethostbyname(servernameorip);
     if (host == NULL) {
 	perror("gethostbyname");
-	return 1;
+	//should throw an error
+	// return 1;
     }
 
     /* initialize socket */
     if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
 	perror("socket");
-	return 1;
+	//should throw an error
+	// return 1;
     }
 
     /* initialize server addr */
@@ -42,18 +53,22 @@ return_type make_remote_call(	const char *servernameorip,
     server.sin_addr = *((struct in_addr*) host->h_addr);
 
     //construct procedure call
-    char *procedure_call = *procedure_name + '(';
+    char *procedure_call;
+    char *open_paranthesis = '(';
+    char *close_paranthesis = ')';
+    strcpy (procedure_call, procedure_name);
+    strcat(procedure_call, open_paranthesis);
     va_list arguments;
-    va_start(a_list, nparams);
+    va_start(arguments, nparams);
     for (int i = 0; i < nparams; ++i) {
-    	strcat(procedure_call, va_arg(arguments, (void *)));
+    	// strcat(procedure_call, va_arg(arguments, (void *)));
     }
-    strcat(procedure_call, ')');
+    strcat(procedure_call, close_paranthesis);
 
     /* send message */
     if (sendto(s, procedure_call, sizeof(procedure_call), 0, (struct sockaddr *) &server, len) == -1) {
 	perror("sendto()");
-	return 1;
+	// return 1;
     }
 
     /* receive echo.
@@ -64,14 +79,26 @@ return_type make_remote_call(	const char *servernameorip,
     if ((n = recvfrom(s, buf, BUF_SIZE, 0, (struct sockaddr *) &server, &len)) != -1) {
     	//received something
     	//return something from here
-	printf("Received from %s:%d: ", 
-		inet_ntoa(server.sin_addr), 
-		ntohs(server.sin_port)); 
-	fflush(stdout);
-	write(1, buf, n);
-	write(1, "\n", 1);
-    }
+    	printf(	"Received from %s:%d: ",	
+    			inet_ntoa(server.sin_addr), 
+				ntohs(server.sin_port)); 
+    	fflush(stdout);
+		write(1, buf, n);
+		write(1, "\n", 1);
 
+    	if (len>BUF_SIZE) {
+    		// make recvfrom call again with bigger BU_SIZE ?? (not sure)
+    		printf("response is bigger than BUF_SIZE");
+    	} else {
+    		//make return_type from reponse buf
+    		return_type response;
+    		response.return_val = buf;
+    		response.return_size = sizeof(buf);
+    		return response;
+    	}
+    }
+    //might have to close the socket before this
+    //probabaly won't reach this point
     close(s);
-    return 0;
+    // return 0;
 }
