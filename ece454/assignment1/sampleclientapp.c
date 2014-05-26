63eb200a7bf4c2153032ca36b9ca41e48921ac4d
Justin Williams <justin.graham49@gmail.com>
2014-05-25 22:27:42 -0400

    Sample code working for server_stub stuff

--- ece454/assignment1/sampleclientapp.c
+++ ece454/assignment1/sampleclientapp.c
@@ -3,14 +3,14 @@
 
 int main()
 {
-    int a = -10, b = 20;
-    return_type ans = make_remote_call("exelinux3.uwaterloo.ca",
-	                               5673,
-				       "addtwo", 2,
-	                               sizeof(int), (void *)(&a),
-	                               sizeof(int), (void *)(&b));
-    int i = *(int *)(ans.return_val);
-    printf("client, got result: %d\n", i);
+	int a = -10, b = 20;
+	return_type ans = make_remote_call("exelinux3.uwaterloo.ca",
+									5673,
+									"addtwo", 2,
+									sizeof(int), (void *)(&a),
+									sizeof(int), (void *)(&b));
+	int i = *(int *)(ans.return_val);
+	printf("client, got result: %d\n", i);
 
-    return 0;
+	return 0;
 }
--- ece454/assignment1/server_stub.c
+++ ece454/assignment1/server_stub.c
@@ -3,7 +3,9 @@
 #include <string.h>
 #include "ece454rpc_types.h"
 
-struct function_node *functions;
+func_node *functions;
+int ret_int;
+return_type r;
 
 /* The following need to be implemented in the server stub */
 
@@ -17,8 +19,8 @@ bool register_procedure(const char *procedure_name,
 	//if it doesn't add it to a linked list or something like that
 	// if it worked
 	bool found = false;
-	struct function_node *itr = malloc(sizeof(*itr));
-	struct function_node *newFunc;
+	func_node *itr = malloc(sizeof(*itr));
+	func_node *newFunc;
 
 	for (itr = functions; itr; itr = itr->next) {
 		if (fnpointer == itr->fnpointer) {
@@ -38,6 +40,32 @@ bool register_procedure(const char *procedure_name,
 	} else {
 		return false;
 	}
+}
+
+return_type add(const int nparams, arg_type* a) {
+	 if(nparams != 2) {
+		/* Error! */
+		r.return_val = NULL;
+		r.return_size = 0;
+		return r;
+    }
+
+    if(a->arg_size != sizeof(int) ||
+       a->next->arg_size != sizeof(int)) {
+		/* Error! */
+		r.return_val = NULL;
+		r.return_size = 0;
+		return r;
+    }
+
+    int i = *(int *)(a->arg_val);
+    int j = *(int *)(a->next->arg_val);
+
+    ret_int = i+j;
+    r.return_val = (void *)(&ret_int);
+    r.return_size = sizeof(int);
+
+    return r;
 }
 
  // launch_server() -- used by the app programmer's server code to indicate that
@@ -63,21 +91,25 @@ void launch_server() {
 	//should not return
 }
 
-int main(int argc, char const *argv[])
-{
-	const char *nm = "asdf";
-	int nParams = 1;
-	fp_type ptr = (fp_type)0x1233123;
-	register_procedure(nm, nParams,ptr);
+int main(int argc, char const *argv[]) {
 
-	const char *nm1 = "asdfP";
-	int nParams1 = 2;
-	fp_type ptr1 = (fp_type)0x1233223;
-	register_procedure(nm1, nParams1,ptr1);
+	register_procedure("addtwo", 2, add);
 
-	struct function_node *itr = malloc(sizeof(*itr));
+	
+	arg_type *args = malloc(sizeof(*args));
+	int arg1 = 1, arg2 = 2;
+
+	args->arg_val = &arg1;
+	args->arg_size = sizeof(int);
+
+	args->next = malloc(sizeof(*args));
+	args->next->arg_val = &arg2;
+	args->next->arg_size = sizeof(int);
+
+	func_node *itr = malloc(sizeof(*itr));
 	for (itr = functions; itr; itr = itr->next) {
-		printf("%s\n", itr->procedure_name);
+		return_type retVal = itr->fnpointer(2, args);
+		printf("Shit returned! %d\n", *((int *)retVal.return_val));
 	}
 
 	return 0;
