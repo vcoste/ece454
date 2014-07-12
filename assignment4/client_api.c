b4d4ee288de67acc626fd8d628414577e97f4e4e
vcoste <vincentcoste_4@hotmail.com>
2014-07-12 15:06:25 -0400

    unmount and other function declarations added

--- assignment4/client_api.c
+++ assignment4/client_api.c
@@ -7,6 +7,7 @@ struct remoteFolderServer {
 };
 
 struct remoteFolderServer server;
+struct fsDirent dent;
 
 int fsMount(const char *srvIpOrDomName, const unsigned int srvPort, const char *localFolderName) {
 	// do similar stuff as ass1 client app
@@ -19,16 +20,78 @@ int fsMount(const char *srvIpOrDomName, const unsigned int srvPort, const char *
 										"fsMount", 1,
 										sizeof(localFolderName), (void *)(localFolderName));
 	int result = *(int*)ans.return_val;
-
 	return result;
 }
 
-// extern int fsUnmount(const char *localFolderName);
-// extern FSDIR* fsOpenDir(const char *folderName);
-// extern int fsCloseDir(FSDIR *);
-// extern struct fsDirent *fsReadDir(FSDIR *);
-// extern int fsOpen(const char *fname, int mode);
-// extern int fsClose(int);
-// extern int fsRead(int fd, void *buf, const unsigned int count);
-// extern int fsWrite(int fd, const void *buf, const unsigned int count);
-// extern int fsRemove(const char *name);
+int fsUnmount(const char *localFolderName) {
+	// 	the counterpart of fsMount() 
+	// 	to unmount a remote ﬁlesystem that is referred to locally by localFolderName. 
+	// 	Returns 0 on success, −1 on failure with errno set appropriately
+	
+	return_type ans = make_remote_call( server.name,
+										server.port ,
+										"fsUnmount", 1,
+										sizeof(localFolderName), (void *)(localFolderName));
+	int result = *(int*)ans.return_val;
+	return result;
+}
+
+FSDIR* fsOpenDir(const char *folderName) {
+    return(opendir(folderName));
+}
+
+int fsCloseDir(FSDIR *folder) {
+    return(closedir(folder));
+}
+
+struct fsDirent *fsReadDir(FSDIR *folder) {
+    const int initErrno = errno;
+    struct dirent *d = readdir(folder);
+
+    if(d == NULL) {
+	if(errno == initErrno) errno = 0;
+	return NULL;
+    }
+
+    if(d->d_type == DT_DIR) {
+	dent.entType = 1;
+    }
+    else if(d->d_type == DT_REG) {
+	dent.entType = 0;
+    }
+    else {
+	dent.entType = -1;
+    }
+
+    memcpy(&(dent.entName), &(d->d_name), 256);
+    return &dent;
+}
+
+int fsOpen(const char *fname, int mode) {
+    int flags = -1;
+
+    if(mode == 0) {
+	flags = O_RDONLY;
+    }
+    else if(mode == 1) {
+	flags = O_WRONLY | O_CREAT;
+    }
+
+    return(open(fname, flags, S_IRWXU));
+}
+
+int fsClose(int fd) {
+    return(close(fd));
+}
+
+int fsRead(int fd, void *buf, const unsigned int count) {
+    return(read(fd, buf, (size_t)count));
+}
+
+int fsWrite(int fd, const void *buf, const unsigned int count) {
+    return(write(fd, buf, (size_t)count)); 
+}
+
+int fsRemove(const char *name) {
+    return(remove(name));
+}
